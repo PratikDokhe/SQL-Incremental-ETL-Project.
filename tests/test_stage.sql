30a4417d139326ddb0d29e64b34ff252f19f8f32
PratikDokhe <79921446+PratikDokhe@users.noreply.github.com>
2025-04-22 17:57:36 +0530

    Create test_stage.sql

---
+++ tests/test_stage.sql
@@ -0,0 +1,14 @@
+/*
+----------------------------------
+Validaty checks stage tables: 
+Verified data loaded successfully or not
+---------------------------------
+*/
+
+select * from stage.customers;
+
+
+select * from stage.orders;
+
+
+select * from stage.products;
